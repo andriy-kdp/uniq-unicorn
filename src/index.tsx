293e44496eeb948c6d1ebe901c4dae98e3b50db8
Andrey <andriy.k.dp@gmail.com>
2022-07-28 10:55:56 +0300

    enable hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme/theme";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 // import "./api/axiosCfg";
@@ -14,12 +14,12 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <GlobalStyles />
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
